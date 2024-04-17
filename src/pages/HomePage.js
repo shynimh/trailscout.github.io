1279c4e1e8dc15cbd85efe465e4082e4fe19fd25
rufazz <rufazz@gmail.com>
2024-04-17 20:40:57 +0000

    newhome

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate, Link } from "react-router-dom"; 
 
 export const HomePage = () => {
-  const navigate = useNavigate(); /
+  const navigate = useNavigate(); 
 
   const trails = [
     {
